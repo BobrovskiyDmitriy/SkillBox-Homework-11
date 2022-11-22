72382e49416dbad1c8f374da9e9d378b33bab0d1
Dmitriy Bobrovskiy <BobrovskiyDU@gmail.com>
2022-11-22 23:40:56 +0300

    22-11-2022

---
+++ HW-11_part-1/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_11_part_1
+{
+    public class Customer
+    {
+
+        /// <summary>
+        /// Фамилия клиента
+        /// </summary>
+        public string Last_name { get; set; }
+
+        /// <summary>
+        /// Имя клиента
+        /// </summary>
+        public string First_name { get; set; }
+
+        /// <summary>
+        /// Отчество клиента
+        /// </summary>
+        public string Middle_name { get; set; }
+
+        /// <summary>
+        /// Телефон клиента
+        /// </summary>
+        public string Phone_numb { get; set; }
+
+        /// <summary>
+        /// Серия и номер паспорта клиента
+        /// </summary>
+        public string Passport { get; set; }
+
+        
+        public Customer(
+            string last_name,
+            string first_name,
+            string middle_name,
+            string phone_numb,
+            string passport)
+        {
+            this.Last_name = last_name;
+            this.First_name = first_name;
+            this.Middle_name = middle_name;
+            this.Phone_numb = phone_numb;
+            this.Passport = passport;
+        }
+
+    }
+
+}
---
+++ HW-11_part-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_11_part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods.MainMenu();
+        }
+    }
+}
